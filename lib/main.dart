8430e2b9a7fe043789185282b8831282ade1e3af
1shabbirhussain <001.shabbirhussain@gmail.com>
2024-03-06 17:16:06 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:asdfg/snackbar.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GetxSnackBar(),
+    );
+  }
+}
